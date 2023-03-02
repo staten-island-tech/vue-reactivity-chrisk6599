c7cbdb13019948c334d0e76abf211fa80a3d183f
GlassPainter <ckwong6599@gmail.com>
2023-03-02 13:05:13 -0500

    3/2/23

---
+++ vue/src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>{{ student }}</h1>
+  <ul>
+    <li v-for="animal in animals" :key="animal">{{ animal }}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      student: "E",
+      animals: ["pig", "cow"],
+    };
+  },
+};
+</script>
