9ee80d4dc2e96392d2e3330ebbb385c78ec585f0
GlassPainter <ckwong6599@gmail.com>
2023-03-07 14:03:13 -0500

    3/7/23

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,29 +1,9 @@
-<template>
-  <h1>{{ student }}</h1>
-  <ul>
-    <li v-for="animal in animals" :key="animal">{{ animal }}</li>
-  </ul>
-  <select v-model="selected">
-    <option>A</option>
-    <option>B</option>
-    <option>C</option>
-  </select>
-  <h1 v-if="selected === A">Correct</h1>
-  <h1 v-else-if="selected === B">Incorrect</h1>
-  <h1 v-else-if="selected === C">W</h1>
-  <!-- <input v-model="text" />
-  <p>{{ text }}</p> -->
-</template>
-
-<script>
-export default {
-  data() {
-    return {
-      student: "E",
-      animals: ["pig", "cow"],
-      selected: "",
-      // text: "",
-    };
-  },
-};
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
 </script>
+
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+</template>
