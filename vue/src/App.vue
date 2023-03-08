860261b953568d6275bb94fc64381faf0047a62f
GlassPainter <ckwong6599@gmail.com>
2023-03-08 14:01:48 -0500

    3/8/23

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -17,7 +17,7 @@ import HelloWorld from './components/HelloWorld.vue'
     </div>
   </header>
 
-  <RouterView />
+  <RouterView></RouterView>
 </template>
 
 <style scoped>
--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,9 +1,33 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+<script>
+export default {
+  name: 'Home',
+  components: {},
+  data() {
+    return {
+      loggedIn: false
+    }
+  },
+  methods: {
+    authState: function () {
+      if (this.loggedIn === false) {
+        this.loggedIn = true
+      } else {
+        this.loggedIn = false
+      }
+    }
+  }
+}
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="home">
+    <button @click="authState" v-if="loggedIn">Log Out</button>
+    <button @click="authState" v-else>Login</button>
+  </div>
 </template>
+
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
