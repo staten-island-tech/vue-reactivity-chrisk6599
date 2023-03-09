026c2871e70223e1adb022856db5fbed3e30f1d6
GlassPainter <ckwong6599@gmail.com>
2023-03-09 14:00:32 -0500

    3/9/2023
    Tutorial #4

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,30 +1,26 @@
+<template>
+  <div class="home">
+    <select v-model="selected">
+      <option>A</option>
+      <option>B</option>
+      <option>C</option>
+    </select>
+    <span>{{ selected }}</span>
+  </div>
+</template>
+
 <script>
 export default {
-  name: 'Home',
+  name: "Home",
   components: {},
   data() {
     return {
-      loggedIn: false
-    }
+      selected: "",
+    };
   },
-  methods: {
-    authState: function () {
-      if (this.loggedIn === false) {
-        this.loggedIn = true
-      } else {
-        this.loggedIn = false
-      }
-    }
-  }
-}
+  methods: {},
+};
 </script>
-
-<template>
-  <div class="home">
-    <button @click="authState" v-if="loggedIn">Log Out</button>
-    <button @click="authState" v-else>Login</button>
-  </div>
-</template>
 
 <style scoped>
 h1 {
