32850660e3ce6c30612d55600ebf244c94d1a9d8
GlassPainter <ckwong6599@gmail.com>
2023-03-14 14:01:02 -0400

    3/14/2023

---
+++ Vue2/src/components/Button.vue
@@ -0,0 +1,17 @@
+<template>
+  <button><slot></slot></button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+};
+</script>
+
+<style>
+button {
+  width: 200px;
+  font-size: 20px;
+  background-color: red;
+}
+</style>
---
+++ Vue2/src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script>
+import Button from "../components/Button.vue";
+export default {
+  name: "Home",
+  components: {
+    Button,
+  },
+};
+</script>
+
+<template>
+  <div class="home">
+    <Button>Button One</Button>
+    <Button>Button Two</Button>
+  </div>
+</template>
