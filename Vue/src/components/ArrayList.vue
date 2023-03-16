06317a3adb6be94ff318f52d81b3a413d86e6246
GlassPainter <ckwong6599@gmail.com>
2023-03-16 14:00:28 -0400

    3/16/2023

---
+++ Vue/src/components/ArrayList.vue
@@ -0,0 +1,14 @@
+<template></template>
+
+<script>
+export default {
+  name: "ArrayList",
+  data() {
+    return {
+      items: [{ name: "a" }, { name: "b" }],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
--- Vue/src/views/HomeView.vue
+++ Vue/src/views/HomeView.vue
@@ -1,12 +1,14 @@
 <script>
 import Button from "../components/Button.vue";
 import Card from "../components/Card.vue";
+import ArrayList from "../components/ArrayList.vue";
 
 export default {
   name: "home",
   components: {
     Button,
     Card,
+    ArrayList,
   },
   methods: {
     hi: function () {
@@ -21,12 +23,15 @@ export default {
 
 <template>
   <div class="home">
-    <Button @button-click="bye">Button One</Button>
+    <ArrayList>
+      <li v-for="item in items">{{ item.name }}</li>
+    </ArrayList>
+    <!-- <Button @button-click="bye">Button One</Button>
     <Button @button-click="hi">Button Two</Button>
     <Card
       title="Hello"
       description="Hi"
       image="https://media.sproutsocial.com/uploads/2017/02/10x-featured-social-media-image-size.png"
-    ></Card>
+    ></Card> -->
   </div>
 </template>
