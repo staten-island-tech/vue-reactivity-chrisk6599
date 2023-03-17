2206be8c6e798c7da14bd8182f1d0a8001d6d648
GlassPainter <ckwong6599@gmail.com>
2023-03-17 14:01:25 -0400

    3/17/2023

--- Vue/src/views/HomeView.vue
+++ Vue/src/views/HomeView.vue
@@ -1,31 +1,60 @@
 <script>
 import Button from "../components/Button.vue";
-import Card from "../components/Card.vue";
+// import Card from "../components/Card.vue";
 import ArrayList from "../components/ArrayList.vue";
 
 export default {
   name: "home",
   components: {
     Button,
-    Card,
+    // Card,
     ArrayList,
   },
   methods: {
-    hi: function () {
-      console.log("hi");
+    add: function () {
+      this.total.push(this.item);
+      console.log(this);
+      console.log(this.total);
     },
-    bye: function () {
-      console.log("bye");
-    },
+    // hi: function () {
+    //   console.log("hi");
+    // },
+    // bye: function () {
+    //   console.log("bye");
+    // },
+  },
+  data() {
+    return {
+      items: [
+        {
+          name: "Rock1",
+          image:
+            "https://media.istockphoto.com/id/1159941628/photo/light-yellow-brown-stone-on-a-white-background.jpg?s=612x612&w=0&k=20&c=Bz9NI4dMHiJuAZK2F8zLWLS-przi4N27eQXRcq-0YF0=",
+          price: 12,
+        },
+        {
+          name: "Rock2",
+          image:
+            "https://media.istockphoto.com/id/1159941628/photo/light-yellow-brown-stone-on-a-white-background.jpg?s=612x612&w=0&k=20&c=Bz9NI4dMHiJuAZK2F8zLWLS-przi4N27eQXRcq-0YF0=",
+          price: 13,
+        },
+      ],
+      total: [],
+    };
   },
 };
 </script>
 
 <template>
   <div class="home">
-    <ArrayList>
-      <li v-for="item in items">{{ item.name }}</li>
-    </ArrayList>
+    <!-- <ArrayList> -->
+    <div v-for="item in items" class="test">
+      <p>{{ item.name }}</p>
+      <img v-bind:src="item.image" />
+      <p>${{ item.price }}</p>
+      <Button @button-click="add">Test</Button>
+    </div>
+    <!-- </ArrayList> -->
     <!-- <Button @button-click="bye">Button One</Button>
     <Button @button-click="hi">Button Two</Button>
     <Card
@@ -35,3 +64,10 @@ export default {
     ></Card> -->
   </div>
 </template>
+
+<style>
+.test {
+  background-color: red;
+  margin-top: 2rem;
+}
+</style>
